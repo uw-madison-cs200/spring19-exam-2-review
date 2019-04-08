bc307f0695ae6914e18d4627b9775d03dd5a6ce5
Sam Vinitsky <samvinitsky@gmail.com>
2019-04-08 18:10:10 -0500

    sam is done

--- src/ArrayListExamples.java
+++ src/ArrayListExamples.java
@@ -11,24 +11,24 @@ public class ArrayListExamples {
 
     	// ****** EXAMPLE 1 *******
     	// initialize
-    	ArrayList<String> arrList = new ArrayList<String>();
-    	
-    	for (int i = 0; i < 10; i++) {
-    		arrList.add("element#" + i);
-    	}
+//    	ArrayList<String> arrList = new ArrayList<String>();
+//    	
+//    	for (int i = 0; i < 10; i++) {
+//    		arrList.add("element#" + i);
+//    	}
     	
     	// ****** EXAMPLE 2 *******
     	// TWO WAYS TO GET ALL THE ELEMENTS
-    	for (int i = 0; i < arrList.size(); i++) {
-    		System.out.println(arrList.get(i));
-    	}
-    	
-    	for (String s : arrList) {
-    		System.out.println(s);
-    	}
+//    	for (int i = 0; i < arrList.size(); i++) {
+//    		System.out.println(arrList.get(i));
+//    	}
+//    	
+//    	for (String s : arrList) {
+//    		System.out.println(s);
+//    	}
     	
     	// ****** EXAMPLE 3 *******
-    	System.out.println(arrList);
+//    	System.out.println(arrList);
     	
     }
 }
