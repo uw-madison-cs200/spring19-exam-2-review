6736576f28b67e3c9584795150fc55444f0ff1f2
JDKWangGuan(街道口网管) <33923108+JiedaokouWangguan@users.noreply.github.com>
2019-04-09 16:24:46 -0500

    Update Methods.java

--- src/Methods.java
+++ src/Methods.java
@@ -9,5 +9,45 @@
  *
  */
 public class Methods {
-	public static void main(String[] args) {}
+    /*
+    concepts:
+    signatures
+    overloading
+    method declaration
+    method call
+    method return
+    parameters
+    instance method
+    static method
+    */
+
+    public static void main(String[] args){
+
+
+    }
+
+    // signatures and overloading
+    public static int methodA(String a, int b, char c) {
+        return 0;
+    }
+
+    public static char methodB(String a, int b, char c) {
+        return 0;
+    }
+
+    public static int methodA(String a, char b) {
+        return 0;
+    }
+
+    public static int methodA(String a, char b, int c) {
+        return 0;
+    }
+
+    public int methodC(String a){
+        return 0;
+    }
+
+
+
 }
+
